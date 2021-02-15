5136a04f1d5ea4eae8d7c398711564e96100016c
techc <bcen@ualberta.ca>
2021-02-14 21:35:59 -0700

    Add black

--- app/src/main/java/com/example/cmput301w21t08/Shape.java
+++ app/src/main/java/com/example/cmput301w21t08/Shape.java
@@ -3,9 +3,5 @@ package com.example.cmput301w21t08;
 public abstract class Shape {
     private int x;
     private int y;
-<<<<<<< HEAD
     String color = "black";
-=======
-    String color = "red";
->>>>>>> 7b679ab8f66c053832cc0d82cfc5b51834ec68ec
 }
